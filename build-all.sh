13c17dd0c042ffe997ba990d0d7448de4a2696eb
Faraj Farook <farajfarook@gmail.com>
2021-09-04 23:06:27 +1000

    Build all script

---
+++ build-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npx ng build @enbiso/utils-url
+npx ng build @enbiso/utils-string
+npx ng build @enbiso/ngrx-route
+npx ng build @enbiso/ngrx-operators
+npx ng build @enbiso/auth
+npx ng build @enbiso/rest-client
